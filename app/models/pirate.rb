841716a7d25aeb470700186d530fc8cfb061282f
ian-ramos <ramos.ianc@gmail.com>
2017-09-19 10:47:30 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+    @@all << self
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    @@all << self
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
